fa26f0f5e09614e5d331f545a12c270b3e5c2f27
nemanjadragun <nemanjadragun92@gmail.com>
2024-03-07 23:21:46 +0100

    chore: refine process

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-console.log("Hello, World from dev!");
+console.log("Hello, World from new dev!");
 
 
 const myFunc = (arg) => {
     return arg
 }
 
+const myFunc2 = (arg) => {
+    return arg + 'from func 2'
+}
+
 module.exports = {
-    myFunc
+    myFunc,
+    myFunc2
 };
